6f5beef8985a34888c5942fe8eaa4a8e883a0964
Dada875 <dsdiary1004@gmail.com>
2024-09-15 05:28:23 +0900

    Add hello command

---
+++ commands/hello.py
@@ -0,0 +1,12 @@
+from discord.ext import commands
+
+class HelloCommand(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='hello')
+    async def hello(self, ctx):
+        await ctx.send('Hello!')
+
+def setup(bot):
+    bot.add_cog(HelloCommand(bot))
